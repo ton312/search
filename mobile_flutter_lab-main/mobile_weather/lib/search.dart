2b87cf475c9c442d1a3454ac39b43bae1325a39e
Alex <lesha-noskov@mail.ru>
2021-11-08 08:28:49 +0300

    search_test

--- mobile_flutter_lab-main/mobile_weather/lib/search.dart
+++ mobile_flutter_lab-main/mobile_weather/lib/search.dart
@@ -1,1 +1,97 @@
 
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:provider/provider.dart';
+import 'dart:developer' as dev;
+
+
+class CitySearchPage extends StatefulWidget {
+  const CitySearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<CitySearchPage> createState() => _CitySearchPageState();
+}
+
+class _CitySearchPageState extends State<CitySearchPage> {
+  final _searchFocus = FocusNode();
+  final _searchController = TextEditingController();
+
+  Future<List<cities.SearchResult>> cityList = Future.value([]);
+
+  @override
+  void initState() {
+    super.initState();
+    _searchFocus.requestFocus();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _fetchCities() {
+    if (_searchController.text.isEmpty) {
+      return;
+    }
+
+    setState(() {
+      cityList = cities.searchCities(_searchController.text, 20);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: NeumorphicAppBar(
+        title: TextField(
+          focusNode: _searchFocus,
+          controller: _searchController,
+          onEditingComplete: _fetchCities,
+          style: const TextStyle(fontSize: 20),
+          decoration: const InputDecoration(
+            hintText: 'Введите город',
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: _fetchCities,
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<cities.SearchResult>>(
+        future: cityList,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            dev.log('Error: ${snapshot.error}');
+            return const Center(child: Text('Не удалось получить результаты'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.separated(
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              var item = snapshot.data![index];
+              var isFavorite = context.read<WeatherModel>().favoriteCities.contains(item.city);
+
+              return GestureDetector(
+                child: ListTile(
+                  title: Text('${item.city} - ${item.country}'),
+                  trailing: Icon(isFavorite ? Icons.star : Icons.star_border),
+                ),
+                onTap: () {
+                  var weather = context.read<WeatherModel>();
+                  weather.currentCity = item.city;
+                  weather.addFavoriteCity(item.city);
+                  Navigator.pop(context);
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- mobile_flutter_lab-main/mobile_weather/lib/settings.dart
+++ mobile_flutter_lab-main/mobile_weather/lib/settings.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
+// ignore: must_be_immutable
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,14 +32,34 @@ class SettingsPage extends StatelessWidget {
                 boxShape:
                     NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
               ),
-              child: Column(
-                children: [
-                  NeumorphicToggle(thumb:const null,
-                  children: [],
-                    
-                  )
-                ],
-              ),
+              child: Column(children: [
+                _Toggle(
+                    title: "Температура",
+                    a: "°C",
+                    b: "°F",
+                    selectedIndex: _Toggle.v1 ? 0 : 1,
+                    onChanged: (index) {
+                      _Toggle.v1 = !_Toggle.v1;
+                    }),
+                const Divider(),
+                _Toggle(
+                    title: "Сила ветра",
+                    a: "м/c",
+                    b: "км/ч",
+                    selectedIndex: _Toggle.v2 ? 0 : 1,
+                    onChanged: (index) {
+                      _Toggle.v2 = !_Toggle.v2;
+                    }),
+                const Divider(),
+                _Toggle(
+                    title: "Давление",
+                    a: "мм.рт.ст.",
+                    b: "гПа",
+                    selectedIndex: _Toggle.v3 ? 0 : 1,
+                    onChanged: (index) {
+                      _Toggle.v3 = !_Toggle.v3;
+                    }),
+              ]),
             ),
           ],
         ),
@@ -47,7 +67,12 @@ class SettingsPage extends StatelessWidget {
     );
   }
 }
-class _Toggle extends StatelessWidget {
+
+class _Toggle extends StatefulWidget {
+  static bool v1 = true;
+  static bool v2 = true;
+  static bool v3 = true;
+
   final String title;
   final String a;
   final String b;
@@ -62,42 +87,52 @@ class _Toggle extends StatelessWidget {
     required this.selectedIndex,
     required this.onChanged,
   });
+  @override
+  State<_Toggle> createState() => _ToggleState();
+}
 
+class _ToggleState extends State<_Toggle> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title),
+      title: Text(widget.title),
       trailing: NeumorphicToggle(
         height: 25,
         width: 130,
-        selectedIndex: selectedIndex,
+        selectedIndex: widget.selectedIndex,
         thumb: Neumorphic(),
-        onChanged: onChanged,
+        onChanged: (Index) {
+          setState(() {
+            widget.onChanged;
+          });
+        },
         children: [
           ToggleElement(
-              background: Center(child: Text(a)),
-              foreground: Neumorphic(
-                style: NeumorphicStyle(
-                  color: NeumorphicTheme.accentColor(context),
-                ),
-                child: Center(
-                  child: Text(
-                    a,
-                    style: NeumorphicTheme.currentTheme(context).textTheme.subtitle2,
-                  ),
-                ),
-              ),
-          ),
-          ToggleElement(
-            background: Center(child: Text(b)),
+            background: Center(child: Text(widget.a)),
             foreground: Neumorphic(
               style: NeumorphicStyle(
                 color: NeumorphicTheme.accentColor(context),
               ),
               child: Center(
                 child: Text(
-                  b,
-                  style: NeumorphicTheme.currentTheme(context).textTheme.subtitle2,
+                  widget.a,
+                  style:
+                      NeumorphicTheme.currentTheme(context).textTheme.subtitle2,
+                ),
+              ),
+            ),
+          ),
+          ToggleElement(
+            background: Center(child: Text(widget.b)),
+            foreground: Neumorphic(
+              style: NeumorphicStyle(
+                color: NeumorphicTheme.accentColor(context),
+              ),
+              child: Center(
+                child: Text(
+                  widget.b,
+                  style:
+                      NeumorphicTheme.currentTheme(context).textTheme.subtitle2,
                 ),
               ),
             ),
@@ -107,3 +142,116 @@ class _Toggle extends StatelessWidget {
     );
   }
 }
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+// class SettingsPage extends StatefulWidget {
+//   const SettingsPage({Key? key}) : super(key: key);
+//   static bool v1 = true;
+//   static bool v2 = true;
+//   static bool v3 = true;
+//   @override
+//   _SettingsState createState() => _SettingsState();
+// }
+
+// class _SettingsState extends State<SettingsPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: NeumorphicAppBar(
+//           leading: IconButton(
+//             onPressed: () => Navigator.pushNamed(context, "/"),
+//             icon: const Icon(
+//               Icons.arrow_back_ios,
+//               size: 20,
+//             ),
+//           ),
+//           title: const Text('Настройки')),
+//       body: Padding(
+//         padding: EdgeInsets.all(MediaQuery.of(context).size.width * 3 / 100),
+//         child: Column(
+//           crossAxisAlignment:
+//               CrossAxisAlignment.start, //выравниваем по горизонтали
+//           children: [
+//             Text(
+//               'Единицы измерения',
+//               style: NeumorphicTheme.currentTheme(context).textTheme.subtitle1,
+//             ),
+//             const SizedBox(height: 20),
+//             Neumorphic(
+//               style: NeumorphicStyle(
+//                 boxShape:
+//                     NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
+//               ),
+//               child: Column(children: [
+//                 toggle(
+//                     context, "Температура", "°C", "°F", SettingsPage.v1 ? 0 : 1,
+//                     (index) {
+//                   SettingsPage.v1 = !SettingsPage.v1;
+//                 }),
+//                 const Divider(),
+//                 toggle(context, "Сила ветра", "м/c", "км/ч",
+//                     SettingsPage.v2 ? 0 : 1, (index) {
+//                   SettingsPage.v2 = !SettingsPage.v2;
+//                 }),
+//                 const Divider(),
+//                 toggle(context, "Давление", "мм.рт.ст.", "гПа",
+//                     SettingsPage.v3 ? 0 : 1, (index) {
+//                   SettingsPage.v3 = !SettingsPage.v3;
+//                 }),
+//               ]),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   ListTile toggle(BuildContext context, String title, a, b, int selectedIndex,
+//       Function(int) onChanged) {
+//     return ListTile(
+//       title: Text(title),
+//       trailing: NeumorphicToggle(
+//         height: 25,
+//         width: 130,
+//         selectedIndex: selectedIndex,
+//         thumb: Neumorphic(),
+//         onChanged: (Index) {
+//           SettingsPage.v1 = !SettingsPage.v1;
+//         },
+//         children: [
+//           ToggleElement(
+//             background: Center(child: Text(a)),
+//             foreground: Neumorphic(
+//               style: NeumorphicStyle(
+//                 color: NeumorphicTheme.accentColor(context),
+//               ),
+//               child: Center(
+//                 child: Text(
+//                   a,
+//                   style:
+//                       NeumorphicTheme.currentTheme(context).textTheme.subtitle2,
+//                 ),
+//               ),
+//             ),
+//           ),
+//           ToggleElement(
+//             background: Center(child: Text(b)),
+//             foreground: Neumorphic(
+//               style: NeumorphicStyle(
+//                 color: NeumorphicTheme.accentColor(context),
+//               ),
+//               child: Center(
+//                 child: Text(
+//                   b,
+//                   style:
+//                       NeumorphicTheme.currentTheme(context).textTheme.subtitle2,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
